3c5a901ab11c4a60d4206428eecf54ebf2cbf320
ashamol <ashamol@users.noreply.github.com>
2016-09-22 11:08:53 +0530

    a file added to test

---
+++ assign.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <link rel="stylesheet" href ="css/main.css">
+      <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Slab" rel="stylesheet">
+   </head>
+   <body>
+      <div class="container">
+      
+         <div class="left-part ">
+            <h1 class="header">Accelerate</h1>
+            <h6 class="header-caption">It's now time to move fast</h6>
+         </div>
+         <div class="right-part "><button class="button-style">Search</button></div>
+         <br/>
+         <div class="right-part ">
+            <h6 class="header-caption">Header Widget Area: Add any widgets you like!</h6>
+         </div>
+         <br/>
+         <br/>
+         <div>
+            <ul>
+               <li><a class="active" href="assign.php">Home</a></li>
+               <li><a href="csvupload.php">CSV Preview</a></li>
+               <li><a href="reg.php">Register</a></li>
+               <li><a href="train.php">Train Management</a></li>
+            </ul>
+         </div>
+         <div class="img-div"><img src="Accelerate/apple-256264_1280.jpg"></div>
+         <div class="img-div"><button class="button-style">Apple above the book</button></div>
+         <br><br><br><br>
+         <div class="img-div">
+            <p class="header-caption">Powder bear claw dessert jelly-o lolipop caramels cotton candy. Jelly-o wafer danish cake. Lolipop gingerbread bear claw unerdwear chocolate cake sweet.</p>
+         </div>
+         <br><br><br>
+         <div class="img-div">
+            <div class="img-gallery">
+               <a target="_blank" href="Accelerate/girl-pose-344322_640-600x330.jpg">
+               <img src="Accelerate/girl-pose-344322_640-600x330.jpg"  width="300" height="200">
+               </a>
+               <div class="article">
+                  <h1 class="article-heading">Clean code</h1>
+                  <p class="article">Dessert Jelly cake low lemondrops jelly beans chocolate bar marshmallow lemon drops jelly beans sesame snaps. Jujubes pastry dragee. Chololate bar marshmallow lemon.</p>
+               </div>
+            </div>
+        
+         <div class="img-gallery">
+            <a target="_blank" href="Accelerate/pretty-351884_640-600x330.jpg">
+            <img src="Accelerate/pretty-351884_640-600x330.jpg"  width="300" height="200">
+            </a>
+            <div class="article">
+               <h1 class="article-heading">Cool Design</h1>
+               <p class="article">Pastry gingerbread wafer powder oat cake carrot cake sugar plum macaroon. Donut toffee tart jelly pudding. Candy canes caramels macaroon cookie dessert macaroon souffle cake.</p>
+            </div>
+         </div>
+         <div class="img-gallery">
+            <a target="_blank" href="Accelerate/girl-354579_640-600x330.jpg">
+            <img src="Accelerate/girl-354579_640-600x330.jpg"  width="300" height="200">
+            </a>
+            <div class="article">
+               <h1 class="article-heading">Nice Support</h1>
+               <p class="article">Tiramisu donut chocolate. Cupcake marshmallow halvah pudding topping cheesecake topping powder. Jujubes applicake gummi bears danish candy gummies dragee pudding.</p>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
